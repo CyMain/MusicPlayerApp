210c7a60a752f32baf22cb987683965632f0a176
CyMain <okikiolacyrus@gmail.com>
2026-09-12 10:25:00 +0100

    feat: setup page-controller and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import './App.css'
 import QueueView from './QueueView/QueueView';
+import Navbar from './Navbar';
+
 
 const App = ()=>{
+  //Handles user data and page control.
+  //I was too lazy, and felt it was unnecessary to make a PageController Component.
+
+
+  const userData = {
+    user_name:"Guest",
+  }
+
+  const [currPage, setCurrPage] = useState("playlist")
+
+  let content = <></>
+  if (currPage == "home"){
+    content = <HomePage/>
+  } else if (currPage == "playlist"){
+    content = <QueueView/>
+  }
+  else if(currPage == " profile"){
+    content = <ProfilePage/>
+  }
+
+  
   return (
     <>
-      <QueueView/>
+      <Navbar/>
+      {content}
     </>
   )
 }
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar(){
+    return(
+        <>
+            <nav className="nav-bar">
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        To Home
+                    </li>
+                    <li className="nav-item">
+                        To Playlist
+                    </li>
+                    <li className="nav-item">
+                        To Profile
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
