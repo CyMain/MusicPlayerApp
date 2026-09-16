8c76b408e0044b16dce36652c05f7623895597d4
CyMain <okikiolacyrus@gmail.com>
2026-09-17 00:32:09 +0100

    chore: setup recents item component

--- frontend/src/Pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,3 +1,4 @@
+import { EllipsisVertical } from "lucide-react";
 import "./ProfilePage.css";
 import demopfp from "/data/demopfp.jpg";
 
@@ -30,10 +31,64 @@ function UserProfileHero(){
     )
 }
 
+// Bottom half of profile page
+
+function RecentSong({ song }){
+    return(
+        <>
+        <div className="recent-song">
+            <figure className="song-cover">
+                <img src={song.cover_link} alt="" />
+            </figure>
+            <div className="song-name">
+                {song.name}
+            </div>
+            <div className="song-options">
+                <EllipsisVertical/>
+            </div>
+        </div>
+        </>
+    )
+}
+
+function RecentSongsList(){
+    const recentSongs = [
+        {
+            id:1,
+            name:"Song Name 1",
+            cover_link:"/data/covers/bad_apple_cover.png",
+        },
+    ]
+
+    return(
+        <>
+            <h3>Recents Played</h3>
+            <ul>
+                {recentSongs.map(
+                    (song)=> <RecentSong song={song} />
+                )}
+            </ul>
+        </>
+    )
+}
+
 export default function ProfilePage(){
     return (
         <>
             <UserProfileHero/>
+            <div className="profile-page-bottom">
+                <div className="left">
+                    <div className="month-details">
+
+                    </div>
+                    <div className="preferences">
+
+                    </div>
+                </div>
+                <div className="right">
+                    <RecentSongsList/>
+                </div>
+            </div>
         </>
     )
 }
