8e0590ac8a024245438e904cc8efc004945e9265
CyMain <okikiolacyrus@gmail.com>
2026-09-16 23:57:35 +0100

    feat: add user profile hero section

--- frontend/src/Pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,39 @@
 import "./ProfilePage.css";
+import demopfp from "/data/demopfp.jpg";
+
+// Hero section of the user profile.
+function UserPFPLarge(){
+    return(
+        <>
+        <figure className="profile-pfp">
+            <img src={demopfp} alt="" />
+        </figure>
+        </>
+    )
+}
+
+function UserProfileHero(){
+    return(
+        <>
+            <div className="user-profile-hero">
+                <UserPFPLarge/>
+                <div className="user-details">
+                    <h2 className="user-name">
+                        Guest
+                    </h2>
+                    <button className="edit-profile-button">
+                        Edit Profile
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default function ProfilePage(){
     return (
         <>
-            <h1>Profile</h1>
+            <UserProfileHero/>
         </>
     )
 }
